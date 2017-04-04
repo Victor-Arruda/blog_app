f500313156b9428bc3b83c42e2bb3cae5290cdb7
Victor Arruda <victor@portabilis.com.br>
2017-04-04 11:50:04 -0300

    hide items form non signed users or not owner

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Listing Articles" do
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    @article1 = Article.create(title: "The first", body: "Lorem ipsum", user: john)
+    @john = User.create(email: "john@example.com", password: "password")
+    @article1 = Article.create(title: "The first", body: "Lorem ipsum", user: @john)
 
-    @article2 = Article.create(title: "The second", body: "Lorem ipsum", user: john)
+    @article2 = Article.create(title: "The second", body: "Lorem ipsum", user: @john)
   end
 
-  scenario "A user list all articles" do
+  scenario "with articles created and user not signed in" do
     visit "/"
 
     expect(page).to have_content(@article1.title)
@@ -17,6 +17,19 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+  end
+
+  scenario "with articles created and user signed in" do
+    login_as(@john)
+    visit "/"
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link("New Article")
   end
 
   scenario "A user has no articles" do
--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Showing an article" do
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    login_as(john)
-    @article = Article.create(title: "The first", body: "Lorem ipsum", user: john)
+    @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "fred@example.com", password: "password")
+    @article = Article.create(title: "The first", body: "Lorem ipsum", user: @john)
   end
 
-  scenario "A user show an article" do
+  scenario "to non-signed user hide the edit and delete buttons" do
     visit "/"
 
     click_link @article.title
@@ -16,5 +16,35 @@ RSpec.feature "Showing an article" do
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
 
+    expect(page).to_not have_link("Edit article")
+    expect(page).to_not have_link("Delete article")
+  end
+
+  scenario "to non-owner user hide the edit and delete buttons" do
+    login_as(@fred)
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).to_not have_link("Edit article")
+    expect(page).to_not have_link("Delete article")
+  end
+
+  scenario "to owner user show the edit and delete buttons" do
+    login_as(@john)
+    visit "/"
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).to have_link("Edit article")
+    expect(page).to have_link("Delete article")
   end
 end
